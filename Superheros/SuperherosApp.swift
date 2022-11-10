5dbb4430c318c61857f88c1ea8c95d1c1c21cc24
Tolba <mahmoudt0lba0111@gmail.com>
2022-11-10 03:41:20 +0200

    Initial Commit

---
+++ Superheros/SuperherosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperherosApp.swift
+//  Superheros
+//
+//  Created by Tolba on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperherosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
